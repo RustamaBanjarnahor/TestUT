e422b0fc043ffdb7d77210fee754f681292b6b61
RustamaBanjarnahor <66253642+RustamaBanjarnahor@users.noreply.github.com>
2020-06-22 10:20:09 +0700

    Update 002.js

--- 002.js
+++ 002.js
@@ -1,1 +1,18 @@
 
+<html>
+<body>
+<p>oddEven:
+<i><b>
+<script type="text/javascript">
+var bil=prompt("Bilangan",0)
+hasil=bil % 2
+if (hasil==0) {
+document.write("Genap")
+}
+else {
+document.write("Ganjil")
+}
+</script>
+</b></i></p>
+</body>
+</html>
