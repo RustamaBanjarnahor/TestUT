856e0908741ea4d20461d0625f33ba85203076d8
RustamaBanjarnahor <66253642+RustamaBanjarnahor@users.noreply.github.com>
2020-06-22 10:49:30 +0700

    Create 003.js

---
+++ 003.js
@@ -0,0 +1,16 @@
+
+<html>
+<body>
+<h1> Rata Rata</h1>
+
+<script type="text/javascript">
+  var numbers = [40, 20, 7, 9, 50];
+  var Max= Math.max.apply(Math,numbers);
+  var Min= Math.min.apply(Math,numbers);
+  document.write.("Nilai :" +numbers+"<br/>");
+  document.write.("Nilai Max :" +Max+"<br/>");
+  document.write.("Nilai Min :" +Min);
+</script>
+
+</body>
+</html>
