e3578d3a5ecec1753dfe4b38eb6016e0c9a2eff6
RustamaBanjarnahor <66253642+RustamaBanjarnahor@users.noreply.github.com>
2020-06-22 10:07:08 +0700

    Update 001.js

--- 001.js
+++ 001.js
@@ -1,1 +1,22 @@
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Nilai</title>
+</head>
+<body>
+    <script>
+        var nilai = prompt("Nilai Grade:");
+        var grade = "";
+
+        if(nilai >= 90) grade = "A"
+        else if(nilai >= 80 ) grade = "B"
+        else if(nilai >=70 ) grade = "C"
+        else if(nilai >=60) grade = "D"
+        else if(nilai <= 59) grade = "E"
+        
+
+        document.write(`<p>Grade: ${grade}</p>`);
+    </script>
+</body>
+</html>
