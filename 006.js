106e39306e86510d482a7b433268b1bcdc3645c3
RustamaBanjarnahor <66253642+RustamaBanjarnahor@users.noreply.github.com>
2020-06-22 14:22:06 +0700

    Update 006.js

--- 006.js
+++ 006.js
@@ -1,9 +1,9 @@
-<HTML>
+<!--<HTML>
 <HEAD>
 <TITLE>Rekursif</TITLE>
 </HEAD>
 <BODY>
-  <script type="text/javascript">
+  <script type="text/javascript">-->
 function faktorial(n) {
 if (n == 0)
 return(1);
@@ -13,7 +13,7 @@ return( n * faktorial(n-1));
 var z = 77;
 document.write("4! = " + faktorial(4));
 
-</SCRIPT>
+<!--</SCRIPT>
 </BODY>
-</HTML>
+</HTML>-->
 
