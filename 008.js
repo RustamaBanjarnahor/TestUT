1073bc5047e2b037313dbd18217604f52c0a5940
RustamaBanjarnahor <66253642+RustamaBanjarnahor@users.noreply.github.com>
2020-06-22 15:58:49 +0700

    Update 008.js

--- 008.js
+++ 008.js
@@ -1,9 +1,16 @@
-function sum(x, y, z) {
-  return x + y + z;
+var x = 0;
+var z = 0;
+labelCancelLoops: while (true) {
+  console.log('Outer loops: ' + x);
+  x += 1;
+  z = 1;
+  while (true) {
+    console.log('Bil: ' + z);
+    z += 1;
+    if (z === 10 && x === 10) {
+      break labelCancelLoops;
+    } else if (z === 10) {
+      break;
+    }
+  }
 }
-const numbers = [1, 2, 3];
-
-console.log(sum(...numbers));
-
-
-console.log(sum.apply(null, numbers));
