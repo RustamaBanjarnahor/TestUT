534d1aa8e605a4042a48e1faec1dd7e9c2319bc0
RustamaBanjarnahor <66253642+RustamaBanjarnahor@users.noreply.github.com>
2020-06-22 19:32:59 +0700

    Update 010.js

--- 010.js
+++ 010.js
@@ -1,17 +1,41 @@
-var json = {
-jsonData: {
-  var name1 =[ "id": 1, "name": "Udin", "age": 12 ],
- 		var name2  = ["id": 2, "name": "Reane", "age": 51 ],
-  		var name3 = [ "id": 3, "name": "Budi", "age": 34 ],
-  		var name4 = [ "id": 4, "name": "Agus", "age": 16 ];
-  		var name5 = [ "id": 5, "name": "Sari", "age": 19 ],
-  		var name6 = [ "id": 6, "name": "Ririn", "age": 20 ],
-  		var name7 = [ "id": 7, "name": "Dessy", "age": 23 ]
-}
-};
+const data = [   
+ 
+  {
+    id: 1,
+    name: 'Udin',
+    age: 12,
+  },
+  {
+    id: 2;
+    name: 'Agus',
+    age: 16,
+  },
+  {
+    id: 3;
+    name: 'Budi',
+    age: 34,
+  },
+  {
+   id: 4;
+    name: 'Agus',
+    age: 16,
+  },
+    {
+   id: 5;
+    name: 'Sari',
+    age: 19,
+  },
+    {
+   id: 6;
+    name: 'Ririn',
+    age: 25,
+  },
+    {
+   id: 7;
+    name: 'Dessi',
+    age: 23,
+  },
+];
 
-for (var key in json.jsonData) {
-    for (var key in json.jsonData[key]) {
-        console.log(json.jsonData[Age][<21])
-    }
-}
+const arr1 = data.filter(d => d.age <21);
+console.log('arr1', arr1, 'arr2', arr2, 'arr4', arr4,'arr5', arr5);
