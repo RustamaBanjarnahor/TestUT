45d0943289cff1ece0a26c314487e6819478a735
RustamaBanjarnahor <66253642+RustamaBanjarnahor@users.noreply.github.com>
2020-06-22 12:20:56 +0700

    Update 004.js

--- 004.js
+++ 004.js
@@ -1,1 +1,27 @@
+<html>
 
+<body>
+  <script type="text/javascript">
+    function checkPalindrome() {
+      var revStr = "";
+      var str = document.getElementById("str").value;
+      var i = str.length;
+      for (var j = i; j >= 0; j--) {
+        revStr = revStr + str.charAt(j);
+      }
+      if (str !== revStr) {
+        alert("True");
+      } else {
+        alert("False");
+      }
+    }
+  </script>
+  <form>
+
+    palindrome <input type="text" id="str" name="string" />
+    <br />
+    <input type="submit" value="Check" onclick="checkPalindrome();" />
+  </form>
+</body>
+
+</html>
