d2245a102e06656cc5d8d5b1efbef75ffca93672
RustamaBanjarnahor <66253642+RustamaBanjarnahor@users.noreply.github.com>
2020-06-22 13:46:52 +0700

    Update 007.js

--- 007.js
+++ 007.js
@@ -1,12 +1,25 @@
-<!DOCTYPE html>
-<html>
-<body>
+<script> 
+function ReverseString(str) { 
+  
+    if(!str || str.length <2 ||  
+            typeof str!== 'string') { 
+        return 'Not valid';  
+    } 
+      
+    // Take empty array revArray 
+    const revArray = []; 
+    const length = str.length - 1; 
+      
+    // Looping from the end 
+    for(let i = length; i >= 0; i--) { 
+        revArray.push(str[i]); 
+    } 
+      
+    // Joining the array elements 
+    return revArray.join(''); 
+} 
+  
+document.write(ReverseString("semua kata-kata")) 
+</script> 
 
-<script>
-var arr=["Semua","kata", "-","kata"];  
-var rev=arr.reverse();
-document.writeln(rev);
-</script>
-
-</body>
-</html>
+ 
