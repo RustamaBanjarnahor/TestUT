c241bc8844dac19c18742dafd3a1fc6012364692
RustamaBanjarnahor <66253642+RustamaBanjarnahor@users.noreply.github.com>
2020-06-22 16:12:20 +0700

    Update 010.js

--- 010.js
+++ 010.js
@@ -1,22 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Mengambil data dari array</title>
-</head>
-<body>
-    <script>
-        // membuat array
-       	var name =[ "id": 1, "name": "Udin", "age": 12 ];
- 		var name  = ["id": 2, "name": "Reane", "age": 51 ];
-  		var name = [ "id": 3, "name": "Budi", "age": 34 ];
-  		var name = [ "id": 4, "name": "Agus", "age": 16 ];
-  		var name = [ "id": 5, "name": "Sari", "age": 19 ];
-  		var name = [ "id": 6, "name": "Ririn", "age": 20 ];
-  		var name = [ "id": 7, "name": "Dessy", "age": 23 ];
-        
-        function name(str) { 
+var json = {
+jsonData: {
+  var name1 =[ "id": 1, "name": "Udin", "age": 12 ],
+ 		var name2  = ["id": 2, "name": "Reane", "age": 51 ],
+  		var name3 = [ "id": 3, "name": "Budi", "age": 34 ],
+  		var name4 = [ "id": 4, "name": "Agus", "age": 16 ];
+  		var name5 = [ "id": 5, "name": "Sari", "age": 19 ],
+  		var name6 = [ "id": 6, "name": "Ririn", "age": 20 ],
+  		var name7 = [ "id": 7, "name": "Dessy", "age": 23 ]
+}
+};
 
-        document.write(name.age[<21]);
-    </script>
-</body>
-</html>
+for (var key in json.jsonData) {
+    for (var key in json.jsonData[key]) {
+        console.log(json.jsonData[Age][<21])
+    }
+}
