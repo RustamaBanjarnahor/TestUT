15f4b20fc8af56319e3007f2570a743e3c44338e
RustamaBanjarnahor <66253642+RustamaBanjarnahor@users.noreply.github.com>
2020-06-22 14:14:02 +0700

    Create 009.js

---
+++ 009.js
@@ -0,0 +1,19 @@
+function removeDuplicates(city) {
+  var x,
+      len=city.length,
+      out=[],
+      obj={};
+ 
+  for (x=0; x<len; x++) {
+    obj[city[x]]=0;
+  }
+  for (x in obj) {
+    out.push(x);
+  }
+  return out;
+}
+var Mycity = ["Jakarta", "Aceh", "Malang", "Medan", "Bontang", "Jogja", "Jakarta", "Bandung", "Malang", "Solo", "Palembang", "Bandung"];
+              
+result = removeDuplicates(Mycity);
+
+console.log(result);
