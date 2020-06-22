645ca28d7ef9d049b54450051c5c6667790dac2d
RustamaBanjarnahor <66253642+RustamaBanjarnahor@users.noreply.github.com>
2020-06-22 19:52:15 +0700

    Update 010.js

--- 010.js
+++ 010.js
@@ -1,41 +1,21 @@
-const data = [   
- 
-  {
-    id: 1,
-    name: 'Udin',
-    age: 12,
-  },
-  {
-    id: 2;
-    name: 'Agus',
-    age: 16,
-  },
-  {
-    id: 3;
-    name: 'Budi',
-    age: 34,
-  },
-  {
-   id: 4;
-    name: 'Agus',
-    age: 16,
-  },
-    {
-   id: 5;
-    name: 'Sari',
-    age: 19,
-  },
-    {
-   id: 6;
-    name: 'Ririn',
-    age: 25,
-  },
-    {
-   id: 7;
-    name: 'Dessi',
-    age: 23,
-  },
-];
 
-const arr1 = data.filter(d => d.age <21);
-console.log('arr1', arr1, 'arr2', arr2, 'arr4', arr4,'arr5', arr5);
+const orang = [
+  
+  { id: 1, name: "Udin", age: 12 },
+  { id: 2, name: "Reane", age: 51 },
+  { id: 3, name: "Budi", age: 34 },
+  { id: 4, name: "Agus", age: 16 },
+  { id: 5, name: "Sari", age: 19 },
+  { id: 6, name: "Ririn", age: 20 },
+  { id: 7, name: "Dessy", age: 23 },
+]
+  
+const orangDiizinkan = []
+
+for (let i = 0; i < orang.length; i++) {
+  if (orang[i].umur <21) {
+    orangDiizinkan.push(orang[i].id);
+  }
+}
+
+alert(orangDiizinkan) 
