7b2a190ef47dcf4c6ff0ceb60add06bf2ecbc2ae
RustamaBanjarnahor <66253642+RustamaBanjarnahor@users.noreply.github.com>
2020-06-22 15:05:15 +0700

    Update 010.js

--- 010.js
+++ 010.js
@@ -1,1 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Mengambil data dari array</title>
+</head>
+<body>
+    <script>
+        // membuat array
+       	var name =[ "id": 1, "name": "Udin", "age": 12 ];
+ 		var name  = ["id": 2, "name": "Reane", "age": 51 ];
+  		var name = [ "id": 3, "name": "Budi", "age": 34 ];
+  		var name = [ "id": 4, "name": "Agus", "age": 16 ];
+  		var name = [ "id": 5, "name": "Sari", "age": 19 ];
+  		var name = [ "id": 6, "name": "Ririn", "age": 20 ];
+  		var name = [ "id": 7, "name": "Dessy", "age": 23 ];
+        
+        function name(str) { 
 
+        document.write(name.age[<21]);
+    </script>
+</body>
+</html>
